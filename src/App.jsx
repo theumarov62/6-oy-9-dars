5e47b037711bc5da02860f96116e7557b368c798
theumarov62 <maxbubaxonxoliqova62@gmail.com>
2025-11-09 09:08:26 +0500

    add b

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import CountryDetail from "./pages/CountryDetail";
+import Home from "./components/Home";
+import CountryDetail from "./components/Detail";
 
 export default function App() {
   return (
